82114cb488c245d3d9e6da1e7da59e765c6d6070
naoyashiga <naoyashiga0@gmail.com>
2015-09-28 21:37:51 +0900

    add ModalViewController

---
+++ RPModalGestureTransition/ModalViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ModalViewController.swift
+//  RPModalGestureTransition
+//
+//  Created by naoyashiga on 2015/09/28.
+//  Copyright © 2015年 naoyashiga. All rights reserved.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
